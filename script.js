25c511c549c9d43106469cda7edf3a9ee18813ed
katreddyvenkateswaraReddy <63310358+katreddyvenkateswaraReddy@users.noreply.github.com>
2023-10-05 10:50:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
-//your code here
 function calculateMinCost() {
-        //your code here
-        // taking input of arrays and store the array as arr
-        const inputElement = document.getElementById("rope-lengths");
-        let arrayOfStrings = inputElement.value.split(",");
-        console.log(arrayOfStrings);        
-
-        let arr = [];
-        for (const iterator of arrayOfStrings) {
-            arr.push(parseInt(iterator));
-        }
-        // console.log(arr);
-        let costOfRopes = 0;
-        while(arr.length > 1){
-            arr.sort((a,b)=>a-b);
-            let val1 = arr.shift();
-            let val2 = arr.shift();
-            let sum = val1+val2;
-            costOfRopes += sum;
-            arr.push(sum); 
-        }
-
-        let result = document.getElementById("result");
-        result.innerText = costOfRopes;
-        return costOfRopes;
-        // console.log(arr);
-        // code logic
-      }  
-
+ const ropeLengthsStr = document.getElementById('rope-lengths').value;
+  const ropeLengthsArr = ropeLengthsStr.split(',').map(Number);
+  const n = ropeLengthsArr.length;
+ 
+  let pq = [];
+  for (let i = 0; i < n; i++) {
+    pq.push(ropeLengthsArr[i]);
+  }
+  pq.sort(function (a, b) { return a - b; });
+ 
+  let res = 0;
+  while (pq.length > 1) {
+    let first = pq.shift();
+    let second = pq.shift();
+    res += first + second;
+    pq.push(first + second);
+    pq.sort(function (a, b) { return a - b; });
+  }
+ 
+  document.getElementById('result').textContent = res;
+}
