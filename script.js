66523c287a2b84a7b88f057f5706e896bacd118c
katreddyvenkateswaraReddy <63310358+katreddyvenkateswaraReddy@users.noreply.github.com>
2023-10-05 10:32:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-function minCostToFormRope(event) {
-  event.preventDefault();
-	var inputElement=document.querySelector('input').value;
-  var arr=inputElement.split(',');
-  arr.sort(function (a,b) {
-    return a-b
-  });
+//your code here
+let ans = 0;
+const btn = document.querySelector(".submitInput");
 
-  var cost=0;
-  while(arr.length>1){
-      var res=Number(arr[0])+ Number(arr[1]);
-      arr.push(res);
-      cost+=res;
-      arr.splice(0,2);
+//function
 
-      arr.sort(function (a,b) {
-        return a-b;
-      });
-      
+function showResult(event) {
+  const values = document.querySelector(".textInput").value;
+  const temp = values.split(",");
+  let arr = [];
+  for (let i = 0; i < temp.length; i++) {
+    arr.push(Number(temp[i]));
   }
-  document.getElementById("result").textContent=cost;
-
+  arr.sort(function (a, b) {
+    return a - b;
+  });
+  while (arr.length > 1) {
+    let first = arr.shift();
+    let second = arr.shift();
+    ans += first + second;
+    arr.push(first + second);
+    arr.sort(function (a, b) {
+      return a - b;
+    });
+  }
+  document.querySelector("#result").textContent = ans;
+  console.log(ans);
 }
+
+btn.addEventListener("click", showResult);
